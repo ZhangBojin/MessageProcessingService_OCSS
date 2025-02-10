dad4da98a6234913cba621fcd45079a6259d7d0f
BorgenZ <bojin.zhang@linexsolutions.com>
2025-02-10 15:27:01 +0800

    new

--- Infrastructure/EFCore/CourseServicesOcssContext.cs
+++ Infrastructure/EFCore/CourseServicesOcssContext.cs
@@ -21,8 +21,7 @@ public partial class CourseServicesOcssContext : DbContext
     public virtual DbSet<CourseAvailability> CourseAvailabilities { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=37.tcp.cpolar.top,10487;Database=CourseServices_OCSS;uid=sa;pwd=zz;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:CourseServicesConn");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
